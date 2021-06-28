62217df97fd38e74f86fd73980a252deadfa329a
Leanne Thompson <78496051+thompsle154@users.noreply.github.com>
2021-06-27 22:34:13 -0600

    updates

--- static/js/app.js
+++ static/js/app.js
@@ -19,3 +19,88 @@ function buildMetaData(sample) {
     });
   });
 }
+
+function buildCharts(sample) {
+    d3.json("samples.json").then((data) => {
+      var sampleData = data.samples;
+      var buildingArray = sampleData.filter(sampleObj => sampleObj.id == sample);
+      var result = buildingArray[0];
+  
+      var otu_ids = result.otu_ids;
+      var otu_labels = result.otu_labels;
+      var sample_values = result.sample_values;
+
+      // Build a Bubble Chart
+    var bubbleChart = {
+        title: "Bacteria Cultures Per Sample",
+        hovermode: "closest",
+        xaxis: { title: "OTU ID" },
+      };
+      var bubbleData = [
+        {
+          x: otu_ids,
+          y: sample_values,
+          text: otu_labels,
+          mode: "markers",
+          marker: {
+            size: sample_values,
+            color: otu_ids,
+            colorscale: "Earth"
+          }
+        }
+      ];
+
+      Plotly.newPlot("bubble", bubbleData, bubbleChart);
+      
+      //Create a horizontal bar chart
+      var yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
+      var barData = [
+        {
+          y: yticks,
+          x: sample_values.slice(0, 10).reverse(),
+          text: otu_labels.slice(0, 10).reverse(),
+          type: "bar",
+          orientation: "h",
+        }
+      ];
+  
+      var chartLayout = {
+        title: "Top 10 Bacteria Cultures Found",
+        margin: { t: 30, l: 150 }
+      };
+  
+      Plotly.newPlot("bar", barData, chartLayout);
+    });
+  };
+
+  function init() {
+    // Grab a reference to the dropdown select element
+    var selectDropdown = d3.select("#selDataset");
+  
+    // Populate the select options by using the list of sample names
+    d3.json("samples.json").then((data) => {
+      var name = data.names;
+  
+      name.forEach((sample) => {
+        selectDropdown
+          .append("option")
+          .text(sample)
+          .property("value", sample);
+      })
+  
+      // Use the sample data from the list to build the plots
+      var sampleData = name[0];
+      buildCharts(sampleData);
+      buildMetaData(sampleData);
+    });
+  };
+  
+  function optionChanged(newSample) {
+    // Fetch new data each time a new sample is selected
+    buildCharts(newSample);
+    buildMetaData(newSample);
+  };
+
+  
+// Initialize the dashboard
+  init() 
